4383f22f1614e0654b3801e011c84cb6d0240474
Haven <woodlandnwater@gmail.com>
2024-11-04 14:48:22 +0000

    minor helath changes

--- Assets/Student Work/Scripts/HealthComponent.cs
+++ Assets/Student Work/Scripts/HealthComponent.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HealthComponent : MonoBehaviour, IDamagable
 {
@@ -10,6 +12,11 @@ public class HealthComponent : MonoBehaviour, IDamagable
     [SerializeField] private float m_MaxHealth;
     private float m_CurrentHealth;
 
+    [Header("Invulnerability Settings")]
+    private bool m_Invulernable;
+    public float m_InvulTimer = 0.5f;
+    private Coroutine m_InvulCoroutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -18,10 +25,28 @@ public class HealthComponent : MonoBehaviour, IDamagable
 
     public void ApplyDamage(float damage, MonoBehaviour attacker)
     {
+        while (m_Invulernable) return;
+        if(m_InvulCoroutine != null)
+        {
+            StopCoroutine(m_InvulCoroutine);
+            m_InvulCoroutine = null;
+        }
         float change = Mathf.Min(m_CurrentHealth, damage);
         m_CurrentHealth -= change;
 
         onDamaged?.Invoke(m_CurrentHealth, m_MaxHealth, change);
         if (m_CurrentHealth <= 0) { onDead?.Invoke(attacker); }
+        if (m_InvulCoroutine == null)
+        {
+             m_InvulCoroutine = StartCoroutine(C_Invulnerable());
+        }
+        
+    }
+
+    IEnumerator C_Invulnerable()
+    {
+        m_Invulernable = true;
+        yield return new WaitForSeconds(m_InvulTimer);
+        m_Invulernable = false;
     }
 }
